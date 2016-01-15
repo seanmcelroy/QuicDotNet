a6f0a5cdfe5a84049428ea859beda4b6ff805026
Sean McElroy <me@seanmcelroy.com>
2016-01-15 01:03:46 -0600

    Initial commit

---
+++ QuicSharp.Test.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace QuicSharp.Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = new Connection();
+            conn.Connect();
+        }
+    }
+}
