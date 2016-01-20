b58591554d196748d465fa4641eda3fddfadebe6
Sean McElroy <me@seanmcelroy.com>
2016-01-19 23:58:49 -0600

    Renaming project from QuicSharp to QuicDotNet

--- QuicDotNet.Test.Console/Properties/AssemblyInfo.cs
+++ QuicDotNet.Test.Console/Properties/AssemblyInfo.cs
@@ -1,20 +1,25 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuicSharp")]
+[assembly: AssemblyTitle("QuicDotNet.Test.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuicSharp")]
+[assembly: AssemblyProduct("QuicDotNet.Test.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45bc577e-1612-423f-a212-9cf6cb347599")]
 
 // Version information for an assembly consists of the following four values:
 //
